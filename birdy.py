cb154afc382dee695186725ed9b42b6d11924411
Danielle Scheele <strangeyellowbird@gmail.com>
2016-08-01 14:47:12 -0500

    comments for planning this far

---
+++ birdy.py
@@ -0,0 +1,7 @@
+# I am still organizing and planning my code before testing and implementation.
+# In my planning, I have built a main menu that prompts a user to enter a choice for what they want to do ( based on the main menu in the requirements)
+# The user's choice will be stored in a variable and will then continue to a specific set of 'if' statements based on the user's choice
+#   example.... if user chooses menu option 1 their choice will prompt a new set of prompts to answer
+#   each menu option has at least one additional set of prompts needed to continue to the desired destination
+#  for now, I thought I will have a text file for all private chirps and a txt file for all public chirps
+# I want to complete thinking through my code before writing my tests and then my implemtation code
