6b7c0f4558edfb99012411a6372402e21f9eed9c
Danielle Scheele <strangeyellowbird@gmail.com>
2016-08-04 15:45:50 -0500

    added classes and Test files/ defined tests

---
+++ TestChirp.py
@@ -0,0 +1,28 @@
+import unittest
+
+class TestChirp(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		pass
+
+	def test_chirp_is_created(self):
+		pass
+
+	def test_chirp_id_is_created(self):
+		pass
+
+	def test_chirp_has_a_user(self):
+		pass
+
+	def test_conversation_id_was_created(self):
+		pass
+
+	def test_time_stamp_was_created(self):
+		pass
+
+	def_test_get_chirp_(self):
+		pass
+
+
+if __name__ == '__main__':
+    unittest.main()
